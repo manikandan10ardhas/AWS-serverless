90c1d2ceb10f221dcd968ffb5aba9d68eb828777
Manikandan Periyasamy <manikandan.periyasamy@impigertech.com>
2022-04-01 10:11:00 +0530

    api gateway in serverless

---
+++ lambdas/getUser.js
@@ -0,0 +1,22 @@
+const Responses = require('./API_Responses')
+
+exports.handler = async(event) =>{
+    console.log("event:::",event)
+
+    if(!event.pathParameters || !event.pathParameters.ID) {
+        //failed without id...
+        return Responses._400({ message: 'ID not found!'})
+    }
+    let ID = event.pathParameters.ID
+    if(data[ID]) {
+        return Responses._200(data[ID])
+    }
+    //id not found cases
+    return Responses._400({ message: 'ID not found!'})
+}
+
+const data = {
+    1: { name: 'Manikandan', age: 25, job: 'Software Engineer'},
+    2: { name: 'Velavan', age: 23, job: 'Medical coder'},
+    3: { name: 'Bharathi chelvan', age: 30, job: 'Mechanical Engineer'}
+}
